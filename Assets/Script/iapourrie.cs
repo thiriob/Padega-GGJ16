c6fa65be48baabba6c8c58cfa3077f314b205cc1
DUDU54330 <duval.lucas@hotmail.fr>
2016-01-31 14:48:34 +0100

    fantome + ia

---
+++ Assets/Script/iapourrie.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class iapourrie : MonoBehaviour {
+
+    public Transform target;
+    public CharacterController c;
+
+    public Vector3 speed;
+    public float detect;
+	
+	void Update ()
+    {
+        if (Vector3.Distance(transform.position, target.position) <= detect)
+            c.Move(-((transform.position - target.transform.position).normalized) * Random.Range(speed.x, speed.y));
+	}
+}
